7761175502db197b56e81dea5df333ab6363dc37
Ethan <zoebom@outlook.com>
2024-09-27 16:41:44 +0100

    Docker file created

--- client/grpc/client.go
+++ client/grpc/client.go
@@ -8,11 +8,12 @@ import (
 	pb "grpc/pb/gen/payment"
 
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
 )
 
 func main() {
 	// Set up a connection to the server.
-	conn, err := grpc.Dial("localhost:50051", grpc.WithInsecure(), grpc.WithBlock())
+	conn, err := grpc.NewClient("localhost:50051", grpc.WithTransportCredentials((insecure.NewCredentials())))
 	if err != nil {
 		log.Fatalf("did not connect: %v", err)
 	}
