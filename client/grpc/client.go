8dd95b0b26a7dbbc9e384890d01de98cdeba37f2
Fangkang <fangkangmi@outlook.com>
2024-09-30 02:20:31 +0100

    Refactor client code to use remote server IP address and port

--- client/grpc/client.go
+++ client/grpc/client.go
@@ -13,39 +13,58 @@ import (
 
 func main() {
 	// Set up a connection to the server.
-	conn, err := grpc.NewClient("localhost:50051", grpc.WithTransportCredentials((insecure.NewCredentials())))
+	conn, err := grpc.NewClient("34.38.55.203:80", grpc.WithTransportCredentials((insecure.NewCredentials())))
 	if err != nil {
 		log.Fatalf("did not connect: %v", err)
 	}
 	defer conn.Close()
 	c := pb.NewPaymentServiceClient(conn)
 
-	// Contact the server and print out its response.
-	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
-	defer cancel()
+	// Create a ticker that ticks every 200 milliseconds (5 times per second)
+	ticker := time.NewTicker(200 * time.Millisecond)
+	defer ticker.Stop()
 
-	// Make a payment
-	paymentReq := &pb.PaymentRequest{
-		UserId:        "user123",
-		Amount:        100.0,
-		Currency:      "USD",
-		PaymentMethod: "credit_card",
-		Description:   "Payment for order #1234",
-		RecipientId:   "recipient456",
-	}
-	paymentRes, err := c.MakePayment(ctx, paymentReq)
-	if err != nil {
-		log.Fatalf("could not make payment: %v", err)
-	}
-	log.Printf("Payment Response: %v", paymentRes)
+	// Create a channel that will send a signal after 1 hour
+	stop := time.After(1 * time.Hour)
 
-	// Get payment status
-	statusReq := &pb.PaymentStatusRequest{
-		TransactionId: paymentRes.TransactionId,
+	for {
+		select {
+		case <-stop:
+			log.Println("Finished sending requests.")
+			return
+		case <-ticker.C:
+			go func() {
+				// Contact the server and print out its response.
+				ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+				defer cancel()
+
+				// Make a payment
+				paymentReq := &pb.PaymentRequest{
+					UserId:        "user123",
+					Amount:        100.0,
+					Currency:      "USD",
+					PaymentMethod: "credit_card",
+					Description:   "Payment for order #1234",
+					RecipientId:   "recipient456",
+				}
+				paymentRes, err := c.MakePayment(ctx, paymentReq)
+				if err != nil {
+					log.Printf("could not make payment: %v", err)
+					return
+				}
+				log.Printf("Payment Response: %v", paymentRes)
+
+				// Get payment status
+				statusReq := &pb.PaymentStatusRequest{
+					TransactionId: paymentRes.TransactionId,
+				}
+				statusRes, err := c.GetPaymentStatus(ctx, statusReq)
+				if err != nil {
+					log.Printf("could not get payment status: %v", err)
+					return
+				}
+				log.Printf("Payment Status: %v", statusRes)
+			}()
+		}
 	}
-	statusRes, err := c.GetPaymentStatus(ctx, statusReq)
-	if err != nil {
-		log.Fatalf("could not get payment status: %v", err)
-	}
-	log.Printf("Payment Status: %v", statusRes)
 }
--- client/rest/client.go
+++ client/rest/client.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"time"
 )
 
 type PaymentRequest struct {
@@ -37,34 +38,51 @@ type PaymentStatusResponse struct {
 }
 
 func main() {
-	// Make a payment
-	paymentReq := PaymentRequest{
-		UserId:        "user123",
-		Amount:        100.0,
-		Currency:      "USD",
-		PaymentMethod: "credit_card",
-		Description:   "Payment for order #1234",
-		RecipientId:   "recipient456",
-	}
-	paymentRes, err := makePayment(paymentReq)
-	if err != nil {
-		log.Fatalf("could not make payment: %v", err)
-	}
-	fmt.Printf("Payment Response: %+v\n", paymentRes)
+	ticker := time.NewTicker(200 * time.Millisecond) // 5 requests per second
+	defer ticker.Stop()
 
-	// Get payment status
-	statusReq := PaymentStatusRequest{
-		TransactionId: paymentRes.TransactionId,
+	stop := time.After(1 * time.Hour)
+
+	for {
+		select {
+		case <-stop:
+			fmt.Println("Finished sending requests.")
+			return
+		case <-ticker.C:
+			go func() {
+				// Make a payment
+				paymentReq := PaymentRequest{
+					UserId:        "user123",
+					Amount:        100.0,
+					Currency:      "USD",
+					PaymentMethod: "credit_card",
+					Description:   "Payment for order #1234",
+					RecipientId:   "recipient456",
+				}
+				paymentRes, err := makePayment(paymentReq)
+				if err != nil {
+					log.Printf("could not make payment: %v", err)
+					return
+				}
+				fmt.Printf("Payment Response: %+v\n", paymentRes)
+
+				// Get payment status
+				statusReq := PaymentStatusRequest{
+					TransactionId: paymentRes.TransactionId,
+				}
+				statusRes, err := getPaymentStatus(statusReq)
+				if err != nil {
+					log.Printf("could not get payment status: %v", err)
+					return
+				}
+				fmt.Printf("Payment Status: %+v\n", statusRes)
+			}()
+		}
 	}
-	statusRes, err := getPaymentStatus(statusReq)
-	if err != nil {
-		log.Fatalf("could not get payment status: %v", err)
-	}
-	fmt.Printf("Payment Status: %+v\n", statusRes)
 }
 
 func makePayment(req PaymentRequest) (*PaymentResponse, error) {
-	url := "http://localhost:8081/make_payment"
+	url := "http://35.233.28.207/make_payment"
 	jsonReq, err := json.Marshal(req)
 	if err != nil {
 		return nil, err
@@ -85,7 +103,7 @@ func makePayment(req PaymentRequest) (*PaymentResponse, error) {
 }
 
 func getPaymentStatus(req PaymentStatusRequest) (*PaymentStatusResponse, error) {
-	url := "http://localhost:8081/get_payment_status"
+	url := "http://35.233.28.207/get_payment_status"
 	jsonReq, err := json.Marshal(req)
 	if err != nil {
 		return nil, err
